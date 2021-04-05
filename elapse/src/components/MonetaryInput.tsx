0688c34df1454520bcb0b8492605138c9e2434c4
Flepp, Simon <Simon.Flepp@zuehlke.com>
2021-04-05 19:27:46 +0200

    Monetary input

--- elapse/src/components/MonetaryInput.tsx
+++ elapse/src/components/MonetaryInput.tsx
@@ -1,9 +1,10 @@
 import { IonItem, IonLabel } from '@ionic/react';
-import React, {ChangeEvent, useRef, useState} from 'react';
+import React, { useRef, useState } from 'react';
 import './MonetaryInput.css';
 import { Currency, MonetaryAmount } from "../model/MonetaryAmount.model";
 import { useSelector } from "react-redux";
 import { RootState } from "../store/reducer";
+import CurrencyInput from "react-currency-input-field";
 
 interface Props {
   invalid: boolean
@@ -14,29 +15,33 @@ interface Props {
 
 const MonetaryInput: React.FC<Props> = (props) => {
 
-  const [amount, setAmount] = useState<string>(props.value.amount === 0 ? '' : props.value.amount + '')
+  const [amount, setAmount] = useState<string>(props.value.amount === 0 ? '0' : props.value.amount + '')
   const currency = useSelector<RootState, Currency>(state => state.settings.preferredCurrency)
   const ref = useRef<HTMLInputElement>(null);
 
-  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value
-    setAmount(value)
+  const onChange = (newValue: string | undefined) => {
+    setAmount(newValue || '0')
 
     props.onChange({
       currency: currency,
-      amount: parseFloat(value)
+      amount: parseFloat(newValue || '0')
     })
   }
   return (
       <>
         <IonItem>
-          <IonLabel onClick={() => ref.current?.focus()} color={props.invalid ? 'danger' : undefined}>Betrag</IonLabel>
-          <span className="currency" onClick={() => ref.current?.focus()}>CHF</span>
-          <input ref={ref} value={ amount } type="number" step="0.01" inputMode="decimal"
-                 style={ { marginLeft: '5px', textAlign: 'right', width: Math.max(amount.toString().length * 12.5, 10) + 'px'} } onChange={ (e) => {
-            onChange(e)
-          } }/>
-
+          <IonLabel onClick={ () => ref.current?.focus() }
+                    color={ props.invalid ? 'danger' : undefined }>Betrag</IonLabel>
+          <CurrencyInput
+              ref={ref}
+              className={ 'monetary-input' }
+              prefix={ currency + ' ' }
+              value={ amount }
+              decimalsLimit={ 2 }
+              groupSeparator={ `'` }
+              allowNegativeValue={ false }
+              onValueChange={ onChange }
+          />
         </IonItem>
       </>
   );
